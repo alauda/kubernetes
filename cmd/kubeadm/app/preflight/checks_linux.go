490158ddc5ac350e465a9aafdbed26d285ad4dac
Tianpeng Wang <tpwang@alauda.io>
2026-05-18 20:48:22 +0800

    kubeadm: skip cgroup v2 preflight checks
    (cherry picked from commit 2f314b927503c09042aafd1188038d146134f8f6)

--- cmd/kubeadm/app/preflight/checks_linux.go
+++ cmd/kubeadm/app/preflight/checks_linux.go
@@ -48,7 +48,6 @@ func (mc MemCheck) Check() (warnings, errorList []error) {
 func addOSValidator(validators []system.Validator, reporter *system.StreamReporter, kubeletVersion string) []system.Validator {
 	validators = append(validators,
 		&system.OSValidator{Reporter: reporter},
-		&system.CgroupsValidator{Reporter: reporter, KubeletVersion: kubeletVersion},
 	)
 	return validators
 }
@@ -70,7 +69,6 @@ func addIPv4Checks(checks []Checker) []Checker {
 
 // addSwapCheck adds a swap check
 func addSwapCheck(checks []Checker) []Checker {
-	checks = append(checks, SwapCheck{})
 	return checks
 }
 
